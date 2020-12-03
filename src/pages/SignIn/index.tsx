64337010ac375a81a8e57813090cc1d8d290483d
Karlos Kelvin <karloskelvinsantos@gmail.com>
2020-12-02 23:29:40 -0300

    Implemented routes in application and implemented signUp intregation with backend

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,6 +1,7 @@
 import React, { useRef, useCallback } from 'react';
+import { Link, useHistory } from 'react-router-dom';
 import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
-import { FormHandles } from '@unform/core';
+import { FormHandles, UnformErrors } from '@unform/core';
 import { Form } from '@unform/web';
 import * as Yup from 'yup';
 
@@ -14,7 +15,7 @@ import logoImg from '../../assets/logo.svg';
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 
-import { Container, Content, Background } from './styles';
+import { Container, Content, AnimationContainer, Background } from './styles';
 
 interface SignInFormData {
   email: string;
@@ -23,8 +24,9 @@ interface SignInFormData {
 
 const SignIn: React.FC = () => {
   const formRef = useRef<FormHandles>(null);
+  const history = useHistory();
 
-  const { user, signIn } = useAuth();
+  const { signIn } = useAuth();
 
   const { addToast } = useToast();
 
@@ -55,53 +57,62 @@ const SignIn: React.FC = () => {
     async (data: SignInFormData) => {
       await validateForm();
 
-      try {
-        await signIn({
-          email: data.email,
-          password: data.password,
-        });
-      } catch (error) {
-        addToast({
-          type: 'error',
-          title: 'Erro na Autenticação',
-          description: 'Ocorreu um erro ao fazer login, cheque as credenciais.',
-        });
+      const errors = formRef.current?.getErrors();
+
+      if (!Object.keys(errors as UnformErrors).length) {
+        try {
+          await signIn({
+            email: data.email,
+            password: data.password,
+          });
+
+          history.push('/dashboard');
+        } catch (error) {
+          addToast({
+            type: 'error',
+            title: 'Erro na Autenticação',
+            description:
+              'Ocorreu um erro ao fazer login, cheque as credenciais.',
+          });
+        }
       }
     },
-    [validateForm, signIn, addToast],
+    [validateForm, signIn, addToast, history],
   );
 
   return (
     <Container>
       <Content>
-        <img src={logoImg} alt="GoBarber" />
+        <AnimationContainer>
+          <img src={logoImg} alt="GoBarber" />
 
-        <Form ref={formRef} onSubmit={handleSubmit}>
-          <h1>Faça seu logon</h1>
+          <Form ref={formRef} onSubmit={handleSubmit}>
+            <h1>Faça seu logon</h1>
 
-          <Input
-            placeholder="E-mail"
-            name="email"
-            icon={FiMail}
-            onChange={validateForm}
-          />
-          <Input
-            type="password"
-            placeholder="Senha"
-            name="password"
-            icon={FiLock}
-            onChange={validateForm}
-          />
+            <Input
+              placeholder="E-mail"
+              name="email"
+              icon={FiMail}
+              onChange={validateForm}
+            />
+            <Input
+              type="password"
+              placeholder="Senha"
+              name="password"
+              icon={FiLock}
+              onChange={validateForm}
+            />
 
-          <Button type="submit">Entrar</Button>
+            <Button type="submit">Entrar</Button>
 
-          <a href="/forgot">Esqueci minha senha</a>
-        </Form>
+            <a href="/forgot">Esqueci minha senha</a>
+          </Form>
 
-        <a href="adasd">
-          <FiLogIn />
-          Criar Conta
-        </a>
+          <Link to="/signup">
+            <FiLogIn />
+            Criar Conta
+          </Link>
+        </AnimationContainer>
       </Content>
       <Background />
     </Container>
--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,21 +1,32 @@
 import React, { useCallback, useRef } from 'react';
+import { Link, useHistory } from 'react-router-dom';
 import { FiArrowLeft, FiMail, FiLock, FiUser } from 'react-icons/fi';
 import { FormHandles } from '@unform/core';
 import { Form } from '@unform/web';
 import * as Yup from 'yup';
 import getValidationErros from '../../utils/getValidationErros';
+import { useToast } from '../../hooks/toast';
+
+import api from '../../services/apiClient';
 
 import logoImg from '../../assets/logo.svg';
 
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 
-import { Container, Content, Background } from './styles';
+import { Container, Content, AnimationContainer, Background } from './styles';
+
+interface SignUpFormData {
+  name: string;
+  email: string;
+  password: string;
+}
 
 const SingUp: React.FC = () => {
   const formRef = useRef<FormHandles>(null);
+  const { addToast } = useToast();
+  const history = useHistory();
 
-  // eslint-disable-next-line @typescript-eslint/ban-types
   const validateForm = useCallback(async () => {
     try {
       formRef.current?.setErrors({});
@@ -38,47 +49,70 @@ const SingUp: React.FC = () => {
     }
   }, []);
 
-  // eslint-disable-next-line @typescript-eslint/ban-types
-  const handleSubmit = useCallback(async () => {
-    await validateForm();
-  }, [validateForm]);
+  const handleSubmit = useCallback(
+    async (data: SignUpFormData) => {
+      await validateForm();
+
+      try {
+        await api.post('/users', data);
+
+        history.push('/');
+
+        addToast({
+          type: 'success',
+          title: 'Cadastro Realizado',
+          description:
+            'Cadastro realizado com sucesso. Você já pode fazer logon no GoBarber.',
+        });
+      } catch (error) {
+        addToast({
+          type: 'error',
+          title: 'Erro no Cadastro',
+          description: 'Ocorreu algum erro no cadastro, tente novamente.',
+        });
+      }
+    },
+    [validateForm, addToast, history],
+  );
 
   return (
     <Container>
       <Background />
       <Content>
-        <img src={logoImg} alt="GoBarber" />
+        <AnimationContainer>
+          <img src={logoImg} alt="GoBarber" />
 
-        <Form ref={formRef} onSubmit={handleSubmit}>
-          <h1>Faça seu cadastro</h1>
+          <Form ref={formRef} onSubmit={handleSubmit}>
+            <h1>Faça seu cadastro</h1>
 
-          <Input
-            placeholder="Nome"
-            name="name"
-            icon={FiUser}
-            onChange={validateForm}
-          />
-          <Input
-            placeholder="E-mail"
-            name="email"
-            icon={FiMail}
-            onChange={validateForm}
-          />
-          <Input
-            type="password"
-            placeholder="Senha"
-            name="password"
-            icon={FiLock}
-            onChange={validateForm}
-          />
+            <Input
+              placeholder="Nome"
+              name="name"
+              icon={FiUser}
+              onChange={validateForm}
+            />
+            <Input
+              placeholder="E-mail"
+              name="email"
+              icon={FiMail}
+              onChange={validateForm}
+            />
+            <Input
+              type="password"
+              placeholder="Senha"
+              name="password"
+              icon={FiLock}
+              onChange={validateForm}
+            />
 
-          <Button type="submit">Cadastrar</Button>
-        </Form>
+            <Button type="submit">Cadastrar</Button>
+          </Form>
 
-        <a href="adasd">
-          <FiArrowLeft />
-          Voltar para Logon
-        </a>
+          <Link to="/">
+            <FiArrowLeft />
+            Voltar para Logon
+          </Link>
+        </AnimationContainer>
       </Content>
     </Container>
   );
